e41abc759e4788c2cf8b5498068a8da926419783
Sankalp Sharma <sankalpsharma@Sankalps-MacBook-Air.local>
2026-02-22 10:20:04 +0530

    interview,resume,cover letter

--- app/layout.js
+++ app/layout.js
@@ -1,44 +1,48 @@
-import { ThemeProvider } from "@/components/theme-provider";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
-import { dark } from '@clerk/themes'
 import { Toaster } from "sonner";
+import Header from "@/components/header";
+import { ThemeProvider } from "@/components/theme-provider";
+import { dark } from "@clerk/themes";
 
+const inter = Inter({ subsets: ["latin"] });
 
-const inter= Inter({subsets: ["latin"]});
 export const metadata = {
-  title: "Elevai - AI Career Coach",
+  title: "ELEVAI - AI Career Coach",
   description: "",
 };
 
 export default function RootLayout({ children }) {
   return (
-      <ClerkProvider appearance={{
-        theme: dark,
-      }} >   
-        <html lang="en" suppressHydrationWarning>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <head>
+          <link rel="icon" href="/logo.png" sizes="any" />
+        </head>
         <body className={`${inter.className}`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
           >
-          {/*header*/}
-          <Header />
-          <main className="min-h-screen">{children}</main>
-          <Toaster richColors />
-          {/*footer*/}
-          <footer className="bg-muted/50 py-12">
-            <div className="container mx-auto px-4 text-center text-gray-200">
-              <p> Made with ❤️ by Sankalp</p>
-            </div>
-          </footer>
-        </ThemeProvider>
-      </body>
-    </html>
-          </ClerkProvider>
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            <Toaster richColors />
+
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>Made with 💗 by Sankalp</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- components/hero.jsx
+++ components/hero.jsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
-import { Button } from "@/components/ui/button";
+import React, { useEffect, useRef } from "react";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { useEffect, useRef } from "react";
 
 const HeroSection = () => {
   const imageRef = useRef(null);
@@ -30,46 +30,41 @@ const HeroSection = () => {
     <section className="w-full pt-36 md:pt-48 pb-10">
       <div className="space-y-6 text-center">
         <div className="space-y-6 mx-auto">
-          <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl 
-          gradient-title">
+          <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient-title animate-gradient">
             Your AI Career Coach for
             <br />
             Professional Success
           </h1>
           <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl">
-            Advance your career with personalized guidance, interview prep and
-            AI-powered tools for job success
+            Advance your career with personalized guidance, interview prep, and
+            AI-powered tools for job success.
           </p>
         </div>
-
         <div className="flex justify-center space-x-4">
           <Link href="/dashboard">
-            <Button className="px-8 ">
+            <Button size="lg" className="px-8">
               Get Started
             </Button>
           </Link>
-          <Link href="https://www.youtube.com/roadsidecoder">
-            <Button className="px-8" variant="outline">
-              Get Started
+          <Link href="https://www.youtube.com/results?search_query=how+to+build+resume+and+cover+letter+using+ai">
+            <Button size="lg" variant="outline" className="px-8">
+              Watch Demo
             </Button>
           </Link>
         </div>
-
         <div className="hero-image-wrapper mt-5 md:mt-0">
           <div ref={imageRef} className="hero-image">
             <Image
-              src={"/banner.jpg"}
-              width={1000}
+              src="/banner.jpg"
+              width={1100}
               height={720}
-              alt="Banner Elevai"
+              alt="Dashboard Preview"
               className="rounded-lg shadow-2xl border mx-auto"
               priority
             />
           </div>
         </div>
       </div>
-
-      <div></div>
     </section>
   );
 };
